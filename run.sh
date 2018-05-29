c4d5f6d4ec5e66691afde2cec749a67543cc642b
Fernando Cristiani <fer5cristiani@gmail.com>
2018-05-29 14:17:12 +0200

    Add run file

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGET=$1
+JALANGI_PATH=$(npm explore jalangi2 -- pwd 2>/dev/null)
+
+node $JALANGI_PATH/src/js/commands/jalangi.js \
+    --inlineSource --inlineIID \
+    --analysis $JALANGI_PATH/src/js/runtime/SMemory.js \
+    --analysis analysis/analysis.js \
+    --analysis analysis/utils.js \
+    $TARGET
