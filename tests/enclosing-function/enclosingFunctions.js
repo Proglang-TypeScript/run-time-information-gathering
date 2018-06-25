4d0cdbec5f23f779e50474a4a132f1105bc81d69
Fernando Cristiani <fer5cristiani@gmail.com>
2018-06-25 21:08:32 +0200

    Add basic test for enclosing functions

---
+++ tests/enclosing-function/enclosingFunctions.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var b = {
+	hello: "world"
+};
+
+function f1(a) {
+	function f2() {
+		b.hello = "world!";
+
+		a.someValue = 98;
+
+		function f3(s, c) {
+			if (s && a.someValue < 100 && c.someValue < 100) {
+				a.anotherValue = 1000;
+				return a.anotherValue;
+			}
+		}
+
+		return f3("myString", {someValue: 99});
+	}
+
+	return f2();
+}
+
+var a = {
+	someValue: 99
+};
+
+a = f1(a);
