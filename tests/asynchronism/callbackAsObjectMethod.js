69359a42a74386b064533af2dafcc98f84b4fda3
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-09 19:09:38 +0200

    Add test for callback as an object method

---
+++ tests/asynchronism/callbackAsObjectMethod.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function AsyncObj() {
+	this.callback = function() {};
+
+	this.doSomethingAsync = function(a) {
+		var dis = this;
+		setTimeout(function() {
+			dis.callback(a);
+		}, 300);
+	};
+}
+
+var a = new AsyncObj();
+a.callback = function(a) {
+	return a.someValue < 100;
+};
+
+a.doSomethingAsync({someValue: 100});
