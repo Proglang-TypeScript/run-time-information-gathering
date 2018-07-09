ae85130b81560c2b40e10fcf9c3da28a6272ac04
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-09 21:49:42 +0200

    Change indentation removing spaces

--- utils/functionContainer.js
+++ utils/functionContainer.js
@@ -4,28 +4,28 @@
 
 (function(exp) {
 
-    function FunctionContainer(functionId, name) {
-        this.functionId = functionId;
-        this.functionName = name;
+	function FunctionContainer(functionId, name) {
+		this.functionId = functionId;
+		this.functionName = name;
 
-        this.iid = null;
-        this.isConstructor = null;
-        this.isMethod = null;
-        this.args = {};
-        this.declarationEnclosingFunctionId = null;
+		this.iid = null;
+		this.isConstructor = null;
+		this.isMethod = null;
+		this.args = {};
+		this.declarationEnclosingFunctionId = null;
 
-        this.addArgumentContainer = function(argumentIndex, argumentContainer) {
-            if (!(argumentIndex in this.args)) {
-                this.args[argumentIndex] = argumentContainer;
-            } else {
-                this.args[argumentIndex].interactions = this.args[argumentIndex].interactions.concat(argumentContainer.interactions);
-            }
-        };
+		this.addArgumentContainer = function(argumentIndex, argumentContainer) {
+			if (!(argumentIndex in this.args)) {
+				this.args[argumentIndex] = argumentContainer;
+			} else {
+				this.args[argumentIndex].interactions = this.args[argumentIndex].interactions.concat(argumentContainer.interactions);
+			}
+		};
 
-        this.getArgumentContainer = function(argumentIndex) {
-            return this.args[argumentIndex];
-        };
-    }
+		this.getArgumentContainer = function(argumentIndex) {
+			return this.args[argumentIndex];
+		};
+	}
 
 	exp.FunctionContainer = FunctionContainer;
 
