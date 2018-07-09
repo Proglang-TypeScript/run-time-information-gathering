a814258d2cacb43d5b0ee6389c9d688caa31a8a2
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-09 21:21:57 +0200

    Add extra case to test setTimeout.js

--- tests/asynchronism/setTimeout.js
+++ tests/asynchronism/setTimeout.js
@@ -4,19 +4,27 @@ function getPropTimeout(a) {
 	function f() {
 			a.hello = "world";
 
-			function f1() {
+			var b = {
+				someValue: 99
+			};
+
+			function f1(b) {
 				a.anotherValue = 100;
 
 				function f2() {
 					if (a.someValue < 100) {
 						a.anotherValue = 1000;
 					}
+
+					if (b.someValue < 100) {
+						b.anotherValue = 1000;
+					}
 				}
 
 				f2();
 			}
 
-			f1();
+			f1(b);
 	}
 
 	setTimeout(
