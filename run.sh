77504e1e8f8f8f2e39c9395bf51e2b4674a9be66
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-11 17:23:16 +0200

    Add absolute path to analysis scripts in run script

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 TARGET=$1
 JALANGI_PATH=$(npm explore jalangi2 -- pwd 2>/dev/null)
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+ROOT_PATH=$SCRIPT_PATH
+
 node $JALANGI_PATH/src/js/commands/jalangi.js \
     --inlineSource --inlineIID \
     --analysis $JALANGI_PATH/src/js/runtime/SMemory.js \
-    --analysis analysis/analysis.js \
-    --analysis analysis/utils.js \
+    --analysis $ROOT_PATH/analysis/analysis.js \
+    --analysis $ROOT_PATH/analysis/utils.js \
     $TARGET
