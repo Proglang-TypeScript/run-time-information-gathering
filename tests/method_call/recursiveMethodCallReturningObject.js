9d6615084d3bab5603be230e380ac2d28da27854
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-22 19:53:41 +0200

    Add test for recursive method call

---
+++ tests/method_call/recursiveMethodCallReturningObject.js
@@ -0,0 +1,61 @@
+"use strict";
+
+function traverse(a) {
+	var l = a;
+	var sum = 0;
+
+	while(l !== null) {
+		sum += l.val;
+
+		if (l.useRight) {
+			l = l.getRight();
+		} else {
+			l = l.getLeft();
+		}
+	}
+
+	return sum;
+}
+
+function Node(val, left, right) {
+	this.val = val;
+	this.useRight = false;
+
+	this.getLeft = function() {
+		return left;
+	};
+
+	this.getRight = function() {
+		return right;
+	};
+}
+
+function AnotherNode(val, left, right) {
+	this.val = val;
+	this.useRight = false;
+
+	this.getLeft = function() {
+		return left;
+	};
+
+	this.getRight = function() {
+		return right;
+	};
+}
+
+// var a10 = new Node(10, null, null);
+// var a9 = new Node(9, a10, null);
+// var a8 = new Node(8, a9, null);
+// var a7 = new Node(7, null, a8);
+// var a6 = new Node(6, a7, null);
+// var a5 = new Node(5, a6, null);
+var a5 = new Node(5, null, null);
+var a4 = new Node(4, null, a5);
+var a3 = new Node(3, a4, null);
+var a2 = new Node(2, a3, null);
+var a1 = new Node(1, a2, null);
+
+a4.useRight = true;
+
+var s = traverse(a1);
+console.log(s);
