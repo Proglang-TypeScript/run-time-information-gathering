465863caff500848cfec3e5cbe9cd2ab5fc2f90d
Fernando Cristiani <fer5cristiani@gmail.com>
2018-07-30 18:50:21 +0200

    Move variable creation to different method

--- analysis/analysisBuilder.js
+++ analysis/analysisBuilder.js
@@ -8,108 +8,117 @@
 		this.sandbox = sandbox;
 		this.runTimeInfo = runTimeInfo;
 
+		var dis = this;
+
 		this.buildCallbacks = function() {
-			var functionsExecutionStack = new (require("../utils/functionsExecutionStack.js")).FunctionsExecutionStack();
-			var sMemoryInterface = new (require("../utils/sMemoryInterface.js")).SMemoryInterface(sandbox.smemory);
-			var mapWrapperObjectsOriginalValues = {};
-
-			var objectSerializer = new (require("../utils/objectSerializer.js")).ObjectSerializer(
-				sMemoryInterface
-			);
-
-			var interactionSerializer = new (require("../utils/interactionSerializer.js")).InteractionSerializer(
-				objectSerializer
-			);
-
-			var argumentContainerFinder = new (require("../utils/argumentContainerFinder.js")).ArgumentContainerFinder(
-				runTimeInfo,
-				sMemoryInterface
-			);
-
-			var interactionFinder = new (require("../utils/interactionFinder.js")).InteractionFinder(
-				runTimeInfo,
-				sMemoryInterface
-			);
-
-			var recursiveInteractionsHandler = new (require("../utils/recursiveInteractionsHandler.js")).RecursiveInteractionsHandler(
-				sMemoryInterface,
-				interactionSerializer
-			);
-
-			var argumentProxyBuilder = new (require("../utils/argumentProxyBuilder.js")).ArgumentProxyBuilder(
-				sMemoryInterface
-			);
-
-			var argumentWrapperObjectBuilder = new (require("../utils/argumentWrapperObjectBuilder.js")).ArgumentWrapperObjectBuilder();
-
-			var functionIdHandler = new (require("../utils/functionIdHandler.js")).FunctionIdHandler();
-
-			var interactionWithResultHandler = new (require("../utils/interactionWithResultHandler.js")).InteractionWithResultHandler(
-				interactionFinder,
-				recursiveInteractionsHandler,
-				sMemoryInterface,
-				argumentContainerFinder
-			);
-
-			var wrapperObjectsHandler = new (require("../utils/wrapperObjectsHandler.js")).WrapperObjectsHandler(
-				sMemoryInterface,
-				argumentWrapperObjectBuilder,
-				argumentProxyBuilder
-			);
+			var variables = buildVariables();
 
 			return {
 				functionEnter: new (require("./callbacks/functionEnter.js")).FunctionEnter(
-					runTimeInfo,
-					functionsExecutionStack,
-					functionIdHandler
+					this.runTimeInfo,
+					variables.functionsExecutionStack,
+					variables.functionIdHandler
 				),
 				functionExit: new (require("./callbacks/functionExit.js")).FunctionExit(
-					functionsExecutionStack
+					variables.functionsExecutionStack
 				),
 				declare: new (require("./callbacks/declare.js")).Declare(
-					runTimeInfo,
-					functionsExecutionStack,
-					argumentContainerFinder,
-					sMemoryInterface
+					this.runTimeInfo,
+					variables.functionsExecutionStack,
+					variables.argumentContainerFinder,
+					variables.sMemoryInterface
 				),
 				invokeFunPre: new (require("./callbacks/invokeFunPre.js")).InvokeFunPre(
-					runTimeInfo,
-					functionsExecutionStack,
-					sMemoryInterface,
-					argumentContainerFinder,
-					functionIdHandler,
-					wrapperObjectsHandler
+					this.runTimeInfo,
+					variables.functionsExecutionStack,
+					variables.sMemoryInterface,
+					variables.argumentContainerFinder,
+					variables.functionIdHandler,
+					variables.wrapperObjectsHandler
 				),
 				invokeFun: new (require("./callbacks/invokeFun.js")).InvokeFun(
-					runTimeInfo,
-					functionsExecutionStack,
-					argumentWrapperObjectBuilder,
-					interactionWithResultHandler
+					this.runTimeInfo,
+					variables.functionsExecutionStack,
+					variables.argumentWrapperObjectBuilder,
+					variables.interactionWithResultHandler
 				),
 				getFieldPre: new (require("./callbacks/getFieldPre.js")).GetFieldPre(
-					functionsExecutionStack,
-					sMemoryInterface,
-					functionIdHandler,
-					interactionWithResultHandler
+					variables.functionsExecutionStack,
+					variables.sMemoryInterface,
+					variables.functionIdHandler,
+					variables.interactionWithResultHandler
 				),
 				putFieldPre: new (require("./callbacks/putFieldPre.js")).PutFieldPre(
-					functionsExecutionStack,
-					sMemoryInterface,
-					argumentContainerFinder,
-					interactionFinder
+					variables.functionsExecutionStack,
+					variables.sMemoryInterface,
+					variables.argumentContainerFinder,
+					variables.interactionFinder
 				),
 				write: new (require("./callbacks/write.js")).Write(
-					functionsExecutionStack,
-					sMemoryInterface
+					variables.functionsExecutionStack,
+					variables.sMemoryInterface
 				),
 				binaryPre: new (require("./callbacks/binaryPre.js")).BinaryPre(
-					wrapperObjectsHandler
+					variables.wrapperObjectsHandler
 				),
 				unaryPre: new (require("./callbacks/unaryPre.js")).UnaryPre(
-					wrapperObjectsHandler
+					variables.wrapperObjectsHandler
 				)
 			};
 		};
+
+		function buildVariables() {
+			var variables = {};
+
+			variables.functionsExecutionStack = new (require("../utils/functionsExecutionStack.js")).FunctionsExecutionStack();
+			variables.sMemoryInterface = new (require("../utils/sMemoryInterface.js")).SMemoryInterface(sandbox.smemory);
+
+			variables.objectSerializer = new (require("../utils/objectSerializer.js")).ObjectSerializer(
+				variables.sMemoryInterface
+			);
+
+			variables.interactionSerializer = new (require("../utils/interactionSerializer.js")).InteractionSerializer(
+				variables.objectSerializer
+			);
+
+			variables.argumentContainerFinder = new (require("../utils/argumentContainerFinder.js")).ArgumentContainerFinder(
+				dis.runTimeInfo,
+				variables.sMemoryInterface
+			);
+
+			variables.interactionFinder = new (require("../utils/interactionFinder.js")).InteractionFinder(
+				dis.runTimeInfo,
+				variables.sMemoryInterface
+			);
+
+			variables.recursiveInteractionsHandler = new (require("../utils/recursiveInteractionsHandler.js")).RecursiveInteractionsHandler(
+				variables.sMemoryInterface,
+				variables.interactionSerializer
+			);
+
+			variables.argumentProxyBuilder = new (require("../utils/argumentProxyBuilder.js")).ArgumentProxyBuilder(
+				variables.sMemoryInterface
+			);
+
+			variables.argumentWrapperObjectBuilder = new (require("../utils/argumentWrapperObjectBuilder.js")).ArgumentWrapperObjectBuilder();
+
+			variables.functionIdHandler = new (require("../utils/functionIdHandler.js")).FunctionIdHandler();
+
+			variables.interactionWithResultHandler = new (require("../utils/interactionWithResultHandler.js")).InteractionWithResultHandler(
+				variables.interactionFinder,
+				variables.recursiveInteractionsHandler,
+				variables.sMemoryInterface,
+				variables.argumentContainerFinder
+			);
+
+			variables.wrapperObjectsHandler = new (require("../utils/wrapperObjectsHandler.js")).WrapperObjectsHandler(
+				variables.sMemoryInterface,
+				variables.argumentWrapperObjectBuilder,
+				variables.argumentProxyBuilder
+			);
+
+			return variables;
+		}
 	}
 
 	exp.AnalysisBuilder = AnalysisBuilder;
