00bfb370bdca4105577da65b73e24f4ea84712dd
Fernando Cristiani <fer5cristiani@gmail.com>
2018-09-09 18:00:52 +0200

    Modify stringify version to avoid circular references

--- utils/interactionSerializer.js
+++ utils/interactionSerializer.js
@@ -7,11 +7,29 @@
 		this.objectSerializer = objectSerializer;
 
 		this.serialize = function(interaction, obj) {
-			var interactionKey = JSON.stringify(interaction);
+			var interactionKey = stringify(interaction);
 			var objSerialized = this.objectSerializer.serializeStructure(obj);
 
 			return interactionKey + "|" + objSerialized;
 		};
+
+		function stringify(o) {
+			var cache = [];
+			let s = JSON.stringify(o, function(key, value) {
+				if (typeof value === 'object' && value !== null) {
+					if (cache.indexOf(value) !== -1) {
+						return;
+					}
+
+					cache.push(value);
+				}
+
+				return value;
+			});
+
+			cache = null;
+			return s;
+		}
 	}
 
 	if (sandbox.utils === undefined) {
