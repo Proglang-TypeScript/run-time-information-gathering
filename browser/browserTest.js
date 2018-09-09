57c94d79903ca2368bfc2ab5c73f81b7cfc65351
Fernando Cristiani <fer5cristiani@gmail.com>
2018-09-09 17:34:27 +0200

    Simplifies browser test jQuery

--- browser/browserTest.js
+++ browser/browserTest.js
@@ -1,17 +1,7 @@
 "use strict";
 
-function f(obj) {
-	if (obj.myValue < 100) {
-		return true;
-	}
-}
-
 $(function() {
 	$("#button_click_me").click(function() {
-		var myObj = {
-			myValue: 90
-		};
-
-		console.log(f(myObj));
+		console.log("on click!");
 	});
 });
