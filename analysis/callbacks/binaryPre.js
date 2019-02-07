e5e8210764793439b735fef2421dfeb20a9cafd1
Fernando Cristiani <fer5cristiani@gmail.com>
2019-02-07 20:00:52 +0100

    Process less than operator

--- analysis/callbacks/binaryPre.js
+++ analysis/callbacks/binaryPre.js
@@ -17,6 +17,45 @@
 			right
 		) {
 
+			let originalLeft = getOriginalValueIfItIsAProxy(left);
+			let originalRight = getOriginalValueIfItIsAProxy(right);
+
+			if (op === "<") {
+				let originalLeftPrimitive = toPrimitive(originalLeft, Number);
+				let originalRightPrimitive = toPrimitive(originalRight, Number);
+
+				let leftConvertedTo = {};
+				let rightConvertedTo = {};
+
+				if (originalLeftPrimitive !== originalLeft) {
+					leftConvertedTo.toPrimitive = {
+						hint: "number",
+						typeOfResult: sandbox.functions.getTypeOf(originalLeftPrimitive)
+					};
+				}
+
+				if (originalRightPrimitive !== originalRight) {
+					rightConvertedTo.toPrimitive = {
+						hint: "number",
+						typeOfResult: sandbox.functions.getTypeOf(originalRightPrimitive)
+					};
+				}
+
+				let typeOfLeft = sandbox.functions.getTypeOf(originalLeft);
+				let typeOfRight = sandbox.functions.getTypeOf(originalRight);
+
+				if (!(typeOfLeft === "string" && typeOfRight === "string")) {
+					leftConvertedTo.convertedTo = "number";
+					leftConvertedTo.isNaN = isNaN(originalLeft);
+
+					rightConvertedTo.convertedTo = "number";
+					rightConvertedTo.isNaN = isNaN(originalRight);
+				} else {
+					leftConvertedTo.convertedTo = "string";
+					rightConvertedTo = "string";
+				}
+			}
+
 			let equalityOperations = [
 				"==",
 				"!=",
@@ -25,8 +64,8 @@
 			];
 
 			if (equalityOperations.indexOf(op) !== -1) {
-				left = replaceValueIfItIsAProxy(left);
-				right = replaceValueIfItIsAProxy(right);
+				left = originalLeft;
+				right = originalRight;
 			}
 
 			return {
@@ -37,7 +76,54 @@
 			};
 		};
 
-		function replaceValueIfItIsAProxy(val) {
+		function isPrimitive(value) {
+			return value !== Object(value);
+		}
+
+		function toPrimitive(input, preferredType) {
+			if (typeof input !== "object" || input === null) {
+				return input;
+			}
+
+			switch (preferredType) {
+				case Number:
+					return toNumber(input);
+				case String:
+					return toString(input);
+				default:
+					if (input instanceof Date) {
+						return toString(input);
+					} else {
+						return toNumber(input);
+					}
+			}
+
+			function toString(input) {
+				if (isPrimitive(input.toString())) {
+					return input.toString();
+				}
+
+				if (isPrimitive(input.valueOf())) {
+					return input.valueOf();
+				}
+
+				throw new TypeError("Cannot convert object to primitive value");
+			}
+
+			function toNumber(input) {
+				if (isPrimitive(input.valueOf())) {
+					return input.valueOf();
+				}
+
+				if (isPrimitive(input.toString())) {
+					return input.toString();
+				}
+
+				throw new TypeError("Cannot convert object to primitive value");
+			}
+		}
+
+		function getOriginalValueIfItIsAProxy(val) {
 			if (dis.wrapperObjectsHandler.objectIsWrapperObject(val)) {
 				return getRealObjectFromProxy(val);
 			} else {
