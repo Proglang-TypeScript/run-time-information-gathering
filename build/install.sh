13620236f80b311991e373a14af2f25626b94410
Fernando Cristiani <fer5cristiani@gmail.com>
2019-03-17 13:31:04 +0100

    Add jalangi custom command

--- build/install.sh
+++ build/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+ROOT_PATH=$SCRIPT_PATH/..
+
 npm install
 
 git clone https://github.com/Samsung/jalangi2.git
 npm install --prefix jalangi2/
 mv jalangi2 node_modules
+
+JALANGI_PATH=$(npm explore jalangi2 --prefix $ROOT_PATH -- pwd 2>/dev/null)
+
+cp $ROOT_PATH/jalangi-commands/jalangi.js $JALANGI_PATH/src/js/commands/jalangi.js
 
 echo "Installation complete!"
