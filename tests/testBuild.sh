230cc97d096d6d9bf782b39e20cb61fa8034ea65
Fernando Cristiani <fer5cristiani@gmail.com>
2019-06-01 11:43:03 +0200

    Avoid faling when container does not exist for the testBuild

--- tests/testBuild.sh
+++ tests/testBuild.sh
@@ -5,8 +5,8 @@ TMP_OUTPUT_FILE=/tmp/output_temp_test_build.json
 
 TEST_BUILD_CONTAINER=test-build-master-mind
 
-docker rm test-build-master-mind &> /dev/null
-docker run --name test-build-master-mind -it -v $SCRIPT_PATH/calculator/calculator.js:/tmp/file.js master-mind-wp3 /tmp/file.js > /dev/null
+docker rm -f test-build-master-mind &> /dev/null
+docker run --name test-build-master-mind -v $SCRIPT_PATH/calculator/calculator.js:/tmp/file.js master-mind-wp3 /tmp/file.js > /dev/null
 docker cp test-build-master-mind:/usr/local/app/output.json $TMP_OUTPUT_FILE
 docker rm test-build-master-mind &> /dev/null
 
