8cd6fd4d53e146d0a2dc0496ed42cc62682698a5
Fernando Cristiani <fer5cristiani@gmail.com>
2021-12-05 22:02:07 +0100

    :wastebasket: Remove unused variable

--- build/install.sh
+++ build/install.sh
@@ -7,6 +7,4 @@ git clone https://github.com/Samsung/jalangi2.git
 npm install --prefix jalangi2/
 mv jalangi2 node_modules
 
-JALANGI_PATH=$(npm explore jalangi2 --prefix $ROOT_PATH -- pwd 2>/dev/null)
-
 $ROOT_PATH/jalangi-commands/updateCommands.sh
