2627b43435e47836b03d6abd3eab7ea88b5dee2a
epeairs <eric.e.peairs@vanderbilt.edu>
2022-08-16 10:44:15 -0600

    Added interaction message

--- utils/argumentContainer.js
+++ utils/argumentContainer.js
@@ -2,16 +2,33 @@
 
 'use strict';
 
+const { produceMessage } = require('./kafka');
+const { nanoid } = require('nanoid');
+
 (function (sandbox) {
   function ArgumentContainer(argumentIndex, name) {
     this.argumentIndex = argumentIndex;
     this.argumentName = name;
+    this.argumentId = nanoid();
 
     this.interactions = [];
 
     this.addInteraction = function (interaction) {
       this.interactions.push(interaction);
+
+      const message = {
+        command: 'add-interaction',
+        data: {
+          argumentId: this.argumentId,
+          interaction,
+        },
+      };
+      
+      // console.log(message);
+      produceMessage(message).catch((err) => console.log(err));
     };
+
+
   }
 
   if (sandbox.utils === undefined) {
