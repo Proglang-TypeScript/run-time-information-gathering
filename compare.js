9093260434f46ea8197fe75213b051c4ed9d69e7
epeairs <eric.e.peairs@vanderbilt.edu>
2022-10-28 17:55:02 +0200

    Can now test without manually removing printed statements from output.json

--- compare.js
+++ compare.js
@@ -1,7 +1,8 @@
 var fs = require('fs'); 
 var file1 = fs.readFileSync('output.json');
 var file2 = fs.readFileSync('output-consumer.json');
-file1 = JSON.stringify(JSON.parse(file1.toString()), null, 4);
+var file1Clean = '{' + file1.toString().split('{').slice(1).join('{');
+file1 = JSON.stringify(JSON.parse(file1Clean), null, 4);
 file2 = JSON.stringify(JSON.parse(file2.toString()), null, 4);
 if (file1 === file2) {
     console.log("They are the same");
