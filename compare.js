5fa1eb335007b79a52e9bb10458a92fa1767c6aa
epeairs <eric.e.peairs@vanderbilt.edu>
2023-01-25 19:09:46 +0100

    Can now compare outputs of individual files

--- compare.js
+++ compare.js
@@ -1,12 +1,48 @@
 var fs = require('fs'); 
-var file1 = fs.readFileSync('output.json');
-var file2 = fs.readFileSync('output-consumer.json');
-var file1Clean = '{\n' + file1.toString().split('\n{\n').slice(1).join('\n{\n');
-file1 = JSON.stringify(JSON.parse(file1Clean), null, 4);
-file2 = JSON.stringify(JSON.parse(file2.toString()), null, 4);
-if (file1 === file2) {
-    console.log("They are the same");
+var file = process.argv[2];
+var failedTest = false;
+
+if (file == 'output') {
+    var file1 = fs.readFileSync(file + '.json');
+    var file2 = fs.readFileSync(file + '-consumer.json');
 }
 else {
-    console.log("They are different");
+    var file1 = fs.readFileSync('test-results/' + file + '-output.json');
+    var file2 = fs.readFileSync('test-results/' + file + '-output-consumer.json');
+}
+file1 = '{\n' + file1.toString().split('\n{\n').slice(1).join('\n{\n');
+
+try {
+    file1 = JSON.stringify(JSON.parse(file1.toString()), null, 4);
+}
+catch {  
+    out = file + ": COMPARE FAIL\n";
+    fs.appendFileSync('test-results.txt', out);
+    failedTest = true;
+    console.log("COMPARE FAIL");
+}
+
+if (!failedTest) {
+    try {
+        file2 = JSON.stringify(JSON.parse(file2.toString()), null, 4);
+    }
+    catch {
+        out = file + ": COMPARE CONSUMER FAIL\n";
+        fs.appendFileSync('test-results.txt', out);
+        failedTest = true;
+        console.log("COMPARE CONSUMER FAIL");
+    }
+}
+
+if (!failedTest) {
+    if (file1 === file2) {
+        out = file + ": SAME\n";
+        fs.appendFileSync('test-results.txt', out);
+        console.log("SAME");
+    }
+    else {
+        out = file + ": DIFFERENT\n";
+        fs.appendFileSync('test-results.txt', out);
+        console.log("DIFFERENT");
+    }
 }
